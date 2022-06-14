fe93ea54b65ac746f1446fc668fa3a4f38512335
lurhkaf11128 <lurhkaf11128@gmail.com>
2022-06-14 14:35:50 +0800

    initial commit

---
+++ TodoListAPI/Data/DonationDBContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using TodoListAPI.Models;
+
+namespace TodoListAPI.Data
+{
+    public partial class DonationDBContext : DbContext
+    {
+        public DonationDBContext()
+        {
+        }
+
+        public DonationDBContext(DbContextOptions<DonationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Dcandidate> Dcandidates { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Dcandidate>(entity =>
+            {
+                entity.ToTable("tablephptest");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Level)
+                    .HasMaxLength(255)
+                    .HasColumnName("level");
+
+                entity.Property(e => e.Content)
+                    .HasMaxLength(255)
+                    .HasColumnName("content");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ TodoListAPI/Models/Dcandidate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoListAPI.Models
+{
+    public partial class Dcandidate
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = null!;
+        public string Level { get; set; } = null!;
+    }
+}
